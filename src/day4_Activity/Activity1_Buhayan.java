a5450d4cd76ccbea6c0487fa8c28186d3d9442c4
Buhayan <sj2.rbuhayan26@joysistvi.edu.ph>
2026-06-23 16:09:44 -0800

    Add Dicussion code day4

---
+++ src/day4_Activity/Activity1_Buhayan.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day4_Activity;
+
+/**
+ *
+ * @author 03
+ */
+public class Activity1_Buhayan {
+    public static void main(String[] args) {
+
+        // Variable declarations and initializations
+        boolean isStudent = true;
+        byte age = 20;
+        short yearLevel = 3;
+        int studentID = 2026001;
+        long population = 117000000L;
+        float height = 5.7f;
+        double grade = 95.75;
+        char section = 'A';
+        String name = "Ryann Buhayan";
+
+        // Printing values
+        System.out.println("Boolean value: " + isStudent);
+        System.out.println("Byte value: " + age);
+        System.out.println("Short value: " + yearLevel);
+        System.out.println("Int value: " + studentID);
+        System.out.println("Long value: " + population);
+        System.out.println("Float value: " + height);
+        System.out.println("Double value: " + grade);
+        System.out.println("Char value: " + section);
+        System.out.println("String value: " + name);
+    }
+}
