c04e97aedb2124f94b7b83061d24f8f081711012
Ryann <sj2.rbuhayan26@joysistvi.edu.ph>
2026-06-25 17:49:35 -0800

    Add Day7 Activity3_ifStatement_Buhayan

--- src/day7_Activity/Activity4_Buhayan.java
+++ src/day7_Activity/Activity4_Buhayan.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package day7_Activity;
 
-/**
- *
- * @author 03
- */
+package day7_Activity;
+import java.util.Scanner;
+
 public class Activity4_Buhayan {
-    
-}
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("== Main Menu ==");
+        System.out.println("1.Start Game");
+        System.out.println("2.Load Game");
+        System.out.println("3.Settings");
+        System.out.println("4.Exit");
+        
+        System.out.print("Enter a Number: ");
+        int choice = input.nextInt();
+        
+            if(choice == 1){
+            System.out.println("You selected Option 1 : Start Game");
+          } else if (choice == 2){
+                System.out.println("You selected Option 2 : Load Game");
+          }  else if(choice == 3){    
+               System.out.println("You selected Option 3 : Settings");
+          }  else if (choice == 4){
+                System.out.println("You selected Option 4 : Exit");    
+          } else{
+                System.out.println("Invalid choice. Please try again.");
+          }
+            
+          }
+    }
+
