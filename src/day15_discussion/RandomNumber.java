3f3fddd92abf4d2f98570c0754d6e0f72fd1816f
Ryann <sj2.rbuhayan26@joysistvi.edu.ph>
2026-07-06 11:16:09 -0800

    Add Lotto numbers game

--- src/day15_discussion/RandomNumber.java
+++ src/day15_discussion/RandomNumber.java
@@ -14,53 +14,67 @@ public class RandomNumber {
     public static void main(String[] args) {
         System.out.println("LOTTO");
 
+        int[] lotto = new int[6];
+
         for (int i = 0; i < 6; i++) {
-            int randomNumber = (int) (Math.random() * 58) + 1;
-            System.out.print("Lotto Numbers :");
-            System.out.println("[" + (i + 1) + "]: " + randomNumber);
-        }
+            int randomnumber;
+            boolean duplicate;
 
-        System.out.println("");
-        System.out.println("BINGO");
-
-        int roll = (int) (Math.random() * 75) + 1;
-        String letter;
-        if (roll <= 15) {
-            letter = "B";
-        } else if (roll <= 30) {
-            letter = "I";
-        } else if (roll <= 45) {
-            letter = "N";
-        } else if (roll <= 60) {
-            letter = "G";
-        } else {
-            letter = "O";
-        }
-    
-        System.out.println("Sa Letrang " + letter + " : " + roll);
-    //Lotto Simulator (1 - 58)
-    //String into integer 
-    System.out.println (
-
-    "");
-    System.out.println (
-    "Sample2");
-        String x = "10";
-
-    int num = Integer.valueOf(x);
-
-    System.out.println (num);
-
-    //downcasting //upcasting
-    int nom = 20;
-
-    double nums = nom; //upcasting
-
-    float y = 10.4f;
-
-    int num2 = (int) y;
-
-}
+            do {
+                randomnumber = (int) (Math.random() * 58) + 1;
+                duplicate = false;
+                
+                for (int j = 0; j < i; j++) {
+                    
+                if (lotto[j] == randomnumber) {
+                    duplicate = true;
+                    break;
+                }
+                }
+            } while (duplicate);
+            lotto [i] = randomnumber;
+            
+            System.out.println("Lotto Number Winner [" + (i + 1 )+ "]:" + randomnumber  );
+            
+            }
+            }
+//        System.out.println("");
+//        System.out.println("BINGO");
+//
+//        int roll = (int) (Math.random() * 75) + 1;
+//        String letter;
+//        if (roll <= 15) {
+//            letter = "B";
+//        } else if (roll <= 30) {
+//            letter = "I";
+//        } else if (roll <= 45) {
+//            letter = "N";
+//        } else if (roll <= 60) {
+//            letter = "G";
+//        } else {
+//            letter = "O";
+//        }
+//        System.out.println("Sa Letrang " + letter + " : " + roll);
+//        //Lotto Simulator (1 - 58)
+//        //String into integer 
+//        System.out.println("");
+//        System.out.println("Sample2");
+//        String x = "10";
+//
+//        int num = Integer.valueOf(x);
+//
+//        System.out.println(num);
+//
+//        //downcasting //upcasting
+//        int nom = 20;
+//
+//        double nums = nom; //upcasting
+//
+//        float y = 10.4f;
+//
+//        int num2 = (int) y;
+//
+    }
 }
 //casting 
 //parsing - convert String into primitive data to vice versa
