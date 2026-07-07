47767ec0b9d1df9467fa6ba410bc75195df0ad7d
Ryann <sj2.rbuhayan26@joysistvi.edu.ph>
2026-07-06 16:39:16 -0800

    Add day 16 Discussion

---
+++ src/day16_discussion/Declarationmethod.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day16_discussion;
+
+/**
+ *
+ * @author 03
+ */
+public class Declarationmethod {
+
+
+
+    public static void main(String[] args) {
+        String name = "Ryann";
+        int age = 20;
+        double salary = 25000.00;
+
+        printName(name, age, salary);
+    }
+
+    public static void printName(String name, int age, double salary) {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
+////invoke the method 
+//        int result = sum(50, 25);
+//        System.out.println("Result of Sum :" + result);
+//    }
+//
+//    public static int sum(int a, int b) {
+//        return a + b;    public static int sum(int a, int b) {
+//
+
+}
